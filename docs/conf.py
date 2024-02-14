601d447eb061e54ced2a260d8856ce02d197697a
antble <anthonyvalcamposano@gmail.com>
2024-02-14 19:54:22 +0000

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,15 +1,15 @@
-# At the top of conf.py
-master_doc = 'index'
-
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
+# this is a trick to make sphinx find the modules in the parent directory
+import os
+import sys
+sys.path.insert(0, os.path.abspath("."))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'interface_builder'
+project = 'interface-builder'
 copyright = '2024, A.V.C.'
 author = 'A.V.C.'
 release = '0.0.1'
@@ -17,15 +17,15 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['myst_parser',"sphinx.ext.autodoc"]
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 html_static_path = ['_static']
