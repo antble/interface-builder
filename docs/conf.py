45b4574b455f3fc5e840418656ee8c3e260d9afb
antble <anthonyvalcamposano@gmail.com>
2024-02-17 22:21:57 +0000

    update conf path direction

--- docs/conf.py
+++ docs/conf.py
@@ -2,10 +2,10 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-# this is a trick to make sphinx find the modules in the parent directory
+
 import os
 import sys
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("."))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
